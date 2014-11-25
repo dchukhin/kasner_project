48c7da3a6dc34db3eee85951e8065c307c0bcfa2
Dmitriy Chukhin <dchukhin@gmail.com>
2014-11-25 18:31:43 -0500

    dummy test for jenkins

--- engine1/tests/test_aaa.py
+++ engine1/tests/test_aaa.py
@@ -4,11 +4,13 @@ from selenium import webdriver
 display = Display(visible=0, size=(800, 600))
 display.start()
 
-# now Firefox will run in a virtual display. 
-# you will not see the browser.
-browser = webdriver.Firefox()
-browser.get('http://www.google.com')
-print browser.title
-browser.quit()
+def test_aaa()
+    # now Firefox will run in a virtual display. 
+    # you will not see the browser.
+    browser = webdriver.Firefox()
+    browser.get('http://www.google.com')
+    print browser.title
+    browser.quit()
+    assert 1 == 1
 
 display.stop()
