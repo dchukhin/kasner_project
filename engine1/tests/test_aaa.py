c5657f2f8b28242bf97bf8df820a13c8aef50a39
Dmitriy Chukhin <dchukhin@gmail.com>
2014-11-25 19:12:18 -0500

    Update test_aaa.py

--- engine1/tests/test_aaa.py
+++ engine1/tests/test_aaa.py
@@ -1,7 +1,7 @@
 from pyvirtualdisplay import Display
 from selenium import webdriver
 
-display = Display(visible=1, size=(800, 600))
+display = Display(visible=0, size=(800, 600))
 display.start()
 
 def test_aaa():
