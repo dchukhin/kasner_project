984e7dcc82fd86ffcff4d5bdacc6a1ff76edd745
Dmitriy <dchukhin@gmail.com>
2015-01-07 23:03:11 -0500

    added navigation tests

---
+++ engine1/tests/test_navigation.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+from xvfbwrapper import Xvfb
+import re
+import pytest
+
+@pytest.fixture(scope="session")
+def w_driver(request):
+    xvfb = Xvfb(width=1280, height=720)
+    xvfb.start()
+    driver=webdriver.Firefox()
+    request.addfinaliser(driver.quit)
+    return driver
+
+@pytest.mark.navigate
+def test_navigates_to_index_page(w_driver):
+    w_driver.get('localhost:8000')
+
+    results=w_driver.page_source
+    text_found=re.search(r'Welcome to the Kasner Micro Search Engine', results)
+    assert(text_found != None)
+
+@pytest.mark.navigate
+def test_navigates_to_about_page(w_driver):
+    w_driver.get('localhost:8000/about')
+
+    results=w_driver.page_source
+    text_found=re.search(r'About the Kasner Search Engine',results)
+    assert(test_found != None)
+
+@pytest.mark.navigate
+def test_navigates_to_index_page_then_about_page(w_driver)
+    w_driver.get('localhost:8000')
+
+    results=w_driver.page_source
+    text_found1=re.search(r'Welcome to the Kasner Micro Search Engine', results)
+
+    w_driver.get('localhost:8000/about')
+    text_found2=re.search(r'About the Kasner Search Engine',results)
+    assert(test_found1 != None and test_found2 != None)
+
+@pytest.mark.navigate
+def test_navigates_to_index_page_then_about_page_then_index_page(w_driver)
+    w_driver.get('localhost:8000')
+    results=w_driver.page_source
+    text_found1=re.search(r'Welcome to the Kasner Micro Search Engine', results)
+
+    w_driver.get('localhost:8000/about')
+    text_found2=re.search(r'About the Kasner Search Engine',results)
+
+    w_driver.get('localhost:8000')
+    results=w_driver.page_source
+    text_found3=re.search(r'Welcome to the Kasner Micro Search Engine', results)
+
+    assert(text_found1 != None and text_found2 != None and text_found3 != None)
