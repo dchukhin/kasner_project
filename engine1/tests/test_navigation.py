d740d3696df058cdf938faad7336e273f65bddac
Dmitriy <dchukhin@gmail.com>
2015-01-08 00:16:19 -0500

    testing links index page and about page

--- engine1/tests/test_navigation.py
+++ engine1/tests/test_navigation.py
@@ -57,3 +57,54 @@ def test_navigates_to_index_page_then_about_page_then_index_page(w_driver):
     assert(text_found1 != None)
     assert(text_found2 != None)
     assert(text_found3 != None)
+
+@pytest.mark.navigate
+def test_navigates_to_index_page_clicks_link_to_about_page_success(w_driver):
+    w_driver.get('localhost:8000')
+    
+    element=w_driver.find_element_by_link_text('About our team').click()
+    results=w_driver.page_source
+    text_found=re.search(r'About the Kasner Search Engine',results)
+
+    assert(text_found != None)
+
+@pytest.mark.navigate
+def test_navigates_to_about_page_clicks_link_to_index_page_success(w_driver):
+    w_driver.get('localhost:8000/about')
+
+    element=w_driver.find_element_by_link_text('back to Kasner').click()
+    results=w_driver.page_source
+    text_found=re.search(r'Welcome to the Kasner Micro Search Engine',results)
+
+    assert(text_found != None)
+
+@pytest.mark.navigate
+def test_navigates_to_index_page_then_about_page_then_index_page_success(w_driver):
+    w_driver.get('localhost:8000')
+
+    element=w_driver.find_element_by_link_text('About our team').click()
+    results=w_driver.page_source
+    text_found1=re.search(r'About the Kasner Search Engine',results)
+
+    element=w_driver.find_element_by_link_text('back to Kasner').click()
+    results=w_driver.page_source
+    text_found2=re.search(r'Welcome to the Kasner Micro Search Engine',results)
+
+    assert(text_found1 != None)
+    assert(text_found2 != None)
+
+@pytest.mark.navigate
+def test_navigates_to_about_page_then_index_page_then_about_page_success(w_driver):
+    w_driver.get('localhost:8000/about')
+
+    element=w_driver.find_element_by_link_text('back to Kasner').click()
+    results=w_driver.page_source
+    text_found1=re.search(r'Welcome to the Kasner Micro Search Engine',results)
+
+    element=w_driver.find_element_by_link_text('About our team').click()
+    results=w_driver.page_source
+    text_found2=re.search(r'About the Kasner Search Engine',results)
+
+    assert(text_found1 != None)
+    assert(text_found2 != None)
+
