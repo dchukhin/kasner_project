5ca7f43e3a6e2650f062e9245a23873872b7de47
Dmitriy <dchukhin@gmail.com>
2015-01-17 17:09:45 -0500

    case insensitive search queries work

--- engine1/tests/test_searches.py
+++ engine1/tests/test_searches.py
@@ -41,7 +41,7 @@ def test_search_o_results_do_not_contain_google_and_yahoo(w_driver):
     google_found=re.search(r'Google', results)
     yahoo_found=re.search(r'Yahoo', results)
 
-    assert (google_found = None) or (yahoo_found = None)
+    assert (google_found == None) or (yahoo_found == None)
 
 def test_search_random_characters(w_driver):
     """Tests whether searching for random characters fails to yield matches"""
@@ -53,5 +53,23 @@ def test_search_random_characters(w_driver):
     text_found=re.search(r'No results', results)
 
     assert text_found != None
+
+def test_search_case_insensitive(w_driver):
+    """Tests whether case insensitive search yields correct results.
+    
+    1.) Search for 'yahoo', find 'Yahoo' in results
+    2.) Search for Yahoo, find 'Yahoo' in results
+    3.) Confirm that results of #1 and #2 are not the same
+    """
+    w_driver.get('localhost:8000')
+    element = w_driver.find_element_by_name('query')
+    element.send_keys('yahoo' + Keys.RETURN)
+    results1=w_driver.page_source
+    
+    element = w_driver.find_element_by_name('query')
+    element.send_keys('Yahoo' + Keys.RETURN)
+    results2=w_driver.page_source
+    
+    assert results1 != results2
 
 
