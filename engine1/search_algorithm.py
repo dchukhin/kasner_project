56cb3d64c1c2b628b92bf19f6984abcee996790a
Dmitriy <dchukhin@gmail.com>
2015-01-19 17:08:43 -0500

    list append used

--- engine1/search_algorithm.py
+++ engine1/search_algorithm.py
@@ -6,17 +6,20 @@ def algorithm(request):
     q=request.GET['query']
     #1.) search for websites whose names match query exactly (case insensitive)
     name_matches=Website.objects.filter(name__iexact=q)
-   
+    websites=list(name_matches)
+
     #2.) search for the wikipedia matches for the query
     wikipedia_search='wikipedia.org/wiki/'+q
     wikipedia_site=Website(name='Wikipedia Results', url=wikipedia_search)
-    wikipedia_matches=[wikipedia_site]
+    wikipedia_matches=wikipedia_site
+    websites.append(wikipedia_matches)
 
     #3.) search for facebook matches
     facebook_search='www.facebook.com/public/'+q
     facebook_site=Website(name='Facebook results', url=facebook_search)
-    facebook_matches=[facebook_site]
-    websites=list(name_matches)+wikipedia_matches+facebook_matches
+    facebook_matches=facebook_site  
+    websites.append(facebook_matches)
+
     return websites
 
 
