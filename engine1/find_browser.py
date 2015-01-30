6607347f0dc6f1cc6fe16d1f2761b5ea422e8476
Dmitriy <dchukhin@gmail.com>
2015-01-29 21:45:02 -0500

    add css for unknown browsers

--- engine1/find_browser.py
+++ engine1/find_browser.py
@@ -3,8 +3,8 @@ def find_browser(request):
     Returns the browser name.
 
     Function is used to determine which CSS to load for each page. CSS should 
-    match the browser. For unknown browsers we return 'firefox' and use the 
-    CSS for firefox.
+    match the browser. For unknown browsers we return 'unknown' and use the 
+    CSS for unknown browsers.
     """
     #get the META data about the browser type
     browser=request.META['HTTP_USER_AGENT']
@@ -16,8 +16,8 @@ def find_browser(request):
     #for chrome
     elif 'chrome' in browser:
         browser='chrome'
-    #for all others we use same as for firefox
+    #for all others
     else:
-        browser='firefox'
+        browser='unknown'
 
     return browser
