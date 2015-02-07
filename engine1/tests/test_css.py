ce563b8af76a16525b47c83bcc3500f8f747eeb0
Dmitriy <dchukhin@gmail.com>
2015-02-07 17:10:26 -0500

    move test for background-color to test_css.py

--- engine1/tests/test_css.py
+++ engine1/tests/test_css.py
@@ -137,6 +137,53 @@ def test_background_image_loads(w_driver):
     actual_image=element.value_of_css_property('background-image')
     assert(expected_image==actual_image)
 
+def test_backgound_color(w_driver):
+    """
+    Tests background colors of pages.
+
+    1.) index page: black
+    2.) results page: white
+    3.) about page: white
+    """
+    #1.) Index Page
+    w_driver.get('localhost:8000')
+    element=w_driver.find_element_by_class_name('container')
+    #the background color is #000000, which maps to rgba(0, 0, 0, 1)
+    expected_color = 'rgba(0, 0, 0, 1)'
+    actual_color = element.value_of_css_property('background-color')
+    assert(expected_color == actual_color)
+    element=w_driver.find_element_by_class_name('container1')
+    #the background color is grey, which maps to rgba(128, 128, 128, 1)
+    expected_color = 'rgba(128, 128, 128, 1)'
+    actual_color = element.value_of_css_property('background-color')
+    assert(expected_color == actual_color)
+
+
+    #2.) Results Page
+    w_driver.get('localhost:8000')
+    #we search for something to pull up the results page
+    element = w_driver.find_element_by_name('query')
+    element.send_keys('o' + Keys.RETURN)
+
+    element=w_driver.find_element_by_class_name('container')
+    #the background color is white, which maps to rgba(255, 255, 255, 1)
+    expected_color = 'rgba(255, 255, 255, 1)'
+    actual_color = element.value_of_css_property('background-color')
+    assert(expected_color == actual_color)
+
+    #3.) About Page
+    w_driver.get('localhost:8000/about')
+    element=w_driver.find_element_by_class_name('container')
+    #the background color is white, which maps to rgba(255, 255, 255, 1)
+    expected_color = 'rgba(255, 255, 255, 1)'
+    actual_color = element.value_of_css_property('background-color')
+    assert(expected_color == actual_color)
+    element=w_driver.find_element_by_class_name('container1')
+    #the background color is white, which maps to rgba(255, 255, 255, 1)
+    expected_color = 'rgba(255, 255, 255, 1)'
+    actual_color = element.value_of_css_property('background-color')
+    assert(expected_color == actual_color)
+
 def test_div_rounded_corners(w_driver, chrome_driver):
     """Tests if div elements have rounded corners.
     Test verifies rounded corners for:
