272d105d34edcb5e7f2ea11a332f1649397110b2
Dmitriy <dchukhin@gmail.com>
2015-03-25 17:55:34 -0400

    include tabs for search results page

--- engine1/static/engine1/app_stats.js
+++ engine1/static/engine1/app_stats.js
@@ -1,11 +1,25 @@
 (function(){
     var app = angular.module("statsApp", []);
-    console.log('aaa');
 
     app.controller('StatsController', function(){
-        console.log('got here');
         this.browsers = browsers;
         this.terms = terms;
+    });
+
+    app.controller('TabController', function(){
+        /*Tab automatically starts with 1, which is Search Terms */
+        this.tab=1;
+
+        /*Method to set tab on user click*/
+        this.setTab = function(tabToSet){
+            this.tab = tabToSet;
+        };
+
+        /*Method to check if the current tab is set. Used to highlight the 
+         * current tab. Returns True/False. */
+        this.isTabSet = function(tabToCheck){
+            return this.tab === tabToCheck;
+        };
     });
 
     var browsers = [
