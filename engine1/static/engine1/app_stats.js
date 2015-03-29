b31a7707c9a705d63a39393843606d7db1035428
Dmitriy <dchukhin@gmail.com>
2015-03-29 16:37:57 -0400

    TabController functionality added to directive that loads element

--- engine1/static/engine1/app_stats.js
+++ engine1/static/engine1/app_stats.js
@@ -5,23 +5,36 @@
         this.browsers = browsers;
         this.terms = terms;
     });
-
+    
+    app.directive('statsTabs', function(){
+        return {
+            restrict: 'E',
+            templateUrl: 'stats-tabs',
+            controller: function(){
+                this.tab = 1;
+                this.setTab = function(tabToSet){
+                    this.tab = tabToSet;
+                };
+                this.isTabSet = function(tabToCheck){
+                    return this.tab === tabToCheck;
+                };
+            },
+            controllerAs: 'tab'
+        };
+    });
+/* 
     app.controller('TabController', function(){
-        /*Tab automatically starts with 1, which is Search Terms */
         this.tab=1;
 
-        /*Method to set tab on user click*/
         this.setTab = function(tabToSet){
             this.tab = tabToSet;
         };
 
-        /*Method to check if the current tab is set. Used to highlight the 
-         * current tab. Returns True/False. */
         this.isTabSet = function(tabToCheck){
             return this.tab === tabToCheck;
         };
     });
-
+*/
     var browsers = [
         {"name":"firefox", "count": 20},
         {"name":"chrome", "count": 10}
