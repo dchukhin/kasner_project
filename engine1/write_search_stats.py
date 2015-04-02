9230b51a4a94c8b46ccfaa364b470873e88341ae
Dmitriy <dchukhin@gmail.com>
2015-04-02 16:41:08 -0400

    create a file to write output form DB in JSON

--- engine1/write_search_stats.py
+++ engine1/write_search_stats.py
@@ -1,6 +1,26 @@
 from models import SearchTerm, SearchBrowser
 import json
 
+def write(for_what, folder):
+    """
+    This function calls the other functions in this file. It seemed easier to 
+    call a single function (this one) from a different file, rather than 
+    calling the other functions in this file from other files. This write 
+    function: 
+        1.) determines which function needs to be called, and calls it, or 
+        2.) raises a ValueError that the arguments for_what and folder were
+        not valid.
+    """
+    if (for_what == 'search_terms' and folder == 'templates'):
+        write_search_terms_stats()
+    elif (for_what == 'browsers' and folder == 'templates'):
+        write_browser_stats()
+    else:
+        raise ValueError ('''At this point this function can only write '''
+                '''json files for search_terms and browsers into the '''
+                '''templates folder. You tried: ''', for_what, '''into '''
+                '''the ''', folder, '''folder.''')
+
 def write_search_terms_stats():
     """
     This function writes a JSON file of the current search terms stats to be 
@@ -11,7 +31,6 @@ def write_search_terms_stats():
     The terms come as a list and each one is named SearchTerm; we change 
     the terms to make them readable, and then write them to the file.
     """
-
     #Open a new file for writing.
     file=open('./engine1/templates/stats_terms.json', 'w')
     #Begin the list.
@@ -25,6 +44,7 @@ def write_search_terms_stats():
     #The final term is blank.
     file.write('{}]')
     file.close()
+    print 'search terms success'
 
 def write_browser_stats():
     """
