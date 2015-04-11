9c0e29bd91c978ae34e3cbdeae1c5f5e7311d33f
Dmitriy <dchukhin@gmail.com>
2015-04-11 14:43:36 -0400

    add instant search for search terms

--- engine1/static/engine1/app_stats.js
+++ engine1/static/engine1/app_stats.js
@@ -37,4 +37,30 @@
         };
     });
     
+    app.filter('searchFilter', function(){
+        /*Filters must return a function. Here array is our array of all the 
+         * search stats, and searchString is the user input.*/
+        return function(array, searchString){
+            /*If user did not enter string, return entire array*/
+            if (!searchString){
+                return array;
+            }
+            /*Otherwise we only want to return terms that include the user 
+             * input.*/
+            var resultArray = [];
+            /*We want the search to be case insensitive.*/
+            searchString = searchString.toLowerCase();
+            /*Every term that includes the searchString gets added to our new
+             * array, resultArray.*/
+            angular.forEach(array, function(term){
+                /*If the searchString is in term.name, add to resultArray*/
+                termNameLowercase = term.name.toLowerCase();
+                if(termNameLowercase.indexOf(searchString) !== -1){
+                    resultArray.push(term);
+                }
+            });
+            return resultArray;
+        };
+    });
+
 })();
